d7cdd089a9e2f77997f278a5c414fba79f72bac9
udbhav100 <i.udbhav@gmail.com>
2020-06-08 20:31:40 +0530

    Added get in achievements.

--- api/rest_apis/api/achievements_view.py
+++ api/rest_apis/api/achievements_view.py
@@ -13,6 +13,17 @@ from rest_framework.views import APIView
 
 class Achievements(APIView):
     permission_classes = [permissions.IsAuthenticated]
+
+    def get(self,request):
+        achievement_list =[]
+        try:
+            achievements1 = achievements.objects.filter(user=request.user)
+            for h in list(achievements1):
+                achievement_list.append({"date":h.date,"title":h.title,"description":h.description})
+            return Response(achievement_list,status=status.HTTP_200_OK)
+        except:
+            return Response([{"failure":"User with given id doesn't have any achievement. try adding one"}],status=status.HTTP_400_BAD_REQUEST)
+
     def post(self,request):
         serial = AchievementSerializer(data=request.data)
         if serial.is_valid():
